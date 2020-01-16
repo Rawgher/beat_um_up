c9756191fbc1eba99663af07d2947ea90abd4a9a
Roger <rdpender3@gmail.com>
2020-01-15 19:49:10 -0800

    added moving camera

---
+++ PompaDroid/Assets/Scripts/CameraBounds.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraBounds : MonoBehaviour
+{
+    public float minVisibleX;
+    public float maxVisibleX;
+    private float minValue;
+    private float maxValue;
+    public float cameraHalfWidth;
+
+    private Camera activeCamera;
+
+    public Transform cameraRoot;
+
+    public Transform leftBounds;
+    public Transform rightBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeCamera = Camera.main;
+
+        cameraHalfWidth = Mathf.Abs(activeCamera.ScreenToWorldPoint(new Vector3(0, 0, 0)).x - activeCamera.ScreenToWorldPoint(new Vector3(Screen.width, 0,0)).x) * 0.5f;
+        minValue = minVisibleX + cameraHalfWidth;
+        maxValue = maxVisibleX - cameraHalfWidth;
+
+        Vector3 position;
+        position = leftBounds.transform.localPosition;
+        position.x = transform.localPosition.x - cameraHalfWidth;
+        leftBounds.transform.localPosition = position;
+
+        position = rightBounds.transform.localPosition;
+        position.x = transform.localPosition.x + cameraHalfWidth;
+        rightBounds.transform.localPosition = position;
+    }
+
+    public void SetXPosition(float x) {
+        Vector3 trans = cameraRoot.position;
+        trans.x = Mathf.Clamp(x, minValue, maxValue);
+        cameraRoot.position = trans;
+    }
+    
+}
---
+++ PompaDroid/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Hero actor;
+    public bool cameraFollows = true;
+    public CameraBounds cameraBounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraBounds.SetXPosition(cameraBounds.minVisibleX);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (cameraFollows) 
+       {
+           cameraBounds.SetXPosition(actor.transform.position.x);
+       } 
+    }
+}
